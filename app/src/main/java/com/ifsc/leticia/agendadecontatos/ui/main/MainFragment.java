cc8b9c116c0ec1622829bafd79198c56056cc92d
LeleMaria1 <leticiaonofre01@gmail.com>
2023-06-17 18:06:01 -0300

    Integração entre telas e exibição de dados fictícios
    Finalizando a integração entre telas e exibindo dados fictícios, modificando a classe MainFragment.
    
    Remoção da linha do arquivo AndroidManifest: android:theme="@style/Theme.AgendaDeContatos.NoActionBar"

--- app/src/main/java/com/ifsc/leticia/agendadecontatos/ui/main/MainFragment.java
+++ app/src/main/java/com/ifsc/leticia/agendadecontatos/ui/main/MainFragment.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,25 +17,42 @@ import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ifsc.leticia.agendadecontatos.R;
+import com.ifsc.leticia.agendadecontatos.data.Contact;
+
 import static com.ifsc.leticia.agendadecontatos.MainActivity.CONTACT_ID;
 import static com.ifsc.leticia.agendadecontatos.MainActivity.NEW_CONTACT;
+
+import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mainViewModel;
 
-    public static MainFragment newInstance() {
-        return new MainFragment();
-    }
+    // adaptor para ser uma usada com a lista de contatos
+    ContactsAdapter contactsAdapter;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        // TODO recyclerView, adapter, itemdivider
-
+        // cria uma referência a recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        // Configura arecycler view para exibir os itens na vertical
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getBaseContext()));
+        // anexa um ItemDecorator personalizado para desenhar divisórias entre os itens da lista
+        recyclerView.addItemDecoration(new ItemDivider(getContext()));
+        // cria um adaptador para preencher os dados da lista
+        // o adaptar tambem vincula uma ação para quando for clicado sobre um contato
+        contactsAdapter = new ContactsAdapter(new ContactsAdapter.ContactClickListener() {
+            @Override
+            public void onClick(int contactID) {
+                // Quando clicado sobre um contato, o método onContactSelected é invocado
+                onContactSelected(contactID);
+            }
+        });
+        recyclerView.setAdapter(contactsAdapter); // vincula o adaptador à lista
         // cria uma referencia ao FAB e vincula uma ação
         FloatingActionButton addButton = view.findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -46,31 +65,24 @@ public class MainFragment extends Fragment {
         return view;
     }
 
-    private void onAddContact() {
-        //cria um pacote de argumentos
-        Bundle arguments = new Bundle();
-        // adiciona o ID do contato como argumento a ser passado ao fragmento
-        // como se trate de um novo contato o valor a ser encaminhado será  NEW_CONTACT
-        arguments.putInt(CONTACT_ID, NEW_CONTACT);
-        //solitica a transição de tela para o fragmento de adicionar um novo contato
-        Navigation.findNavController(getView()).navigate(R.id.action_mainFragment_to_addEditFragment, arguments);
+    private void onContactSelected(int contactID) {
     }
 
-    // chamado quando o contato é selecionado
-    private void onContactSelected(int contactID){
-        //cria um pacote de argumentos
-        Bundle arguments = new Bundle();
-        // adiciona o contactID informado como argumento a ser passado ao fragmento
-        arguments.putInt(CONTACT_ID, contactID);
-        //solitica a transição de tela para o fragmento de detalhes do contato
-        Navigation.findNavController(getView()).navigate(R.id.action_mainFragment_to_detailFragment, arguments);
+    private void onAddContact() {
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
+
+        ArrayList<Contact> list = new ArrayList<Contact>();
+        list.add(new Contact(1,"Fulano", "1234", "a@a.com"));
+        list.add(new Contact(2,"Beltrano", "4567", "b@a.com"));
+        list.add(new Contact(3,"Ciclano", "8888", "c@a.com"));
+        contactsAdapter.setContacts(list);
     }
 
 }
