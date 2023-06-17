49883da3d4300245b9e48aa856ef10d5adde6375
LeleMaria1 <leticiaonofre01@gmail.com>
2023-06-17 17:47:02 -0300

    Manipulação de Listas: Classe Contact

--- app/src/main/java/com/ifsc/leticia/agendadecontatos/data/Contact.java
+++ app/src/main/java/com/ifsc/leticia/agendadecontatos/data/Contact.java
@@ -1,4 +1,31 @@
 package com.ifsc.leticia.agendadecontatos.data;
 
 public class Contact {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Contact(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
 }
